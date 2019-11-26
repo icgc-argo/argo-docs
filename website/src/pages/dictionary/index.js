702c651e587bd3d37a16a71523a01f64929fa83b
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-11-26 17:13:52 -0500

    add infobar

--- website/src/pages/dictionary/index.js
+++ website/src/pages/dictionary/index.js
@@ -17,6 +17,9 @@ import styles from './styles.module.css';
 
 import Typography from '@icgc-argo/uikit/Typography';
 import Select from '@icgc-argo/uikit/form/Select';
+import Button from '@icgc-argo/uikit/Button';
+import DropdownButton from '@icgc-argo/uikit/DropdownButton';
+import Icon from '@icgc-argo/uikit/Icon';
 
 import Select from '@icgc-argo/uikit/form/Select';
 import Button from '@icgc-argo/uikit/Button';
