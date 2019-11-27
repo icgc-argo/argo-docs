8c3acfb5789b5a8090ca42865d67bf5566e8e6dc
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-11-27 08:39:48 -0500

    styling

--- website/src/pages/dictionary/index.js
+++ website/src/pages/dictionary/index.js
@@ -42,7 +42,16 @@ import Button from '@icgc-argo/uikit/Button';
 import DropdownButton from '@icgc-argo/uikit/DropdownButton';
 import Icon from '@icgc-argo/uikit/Icon';
 
-const DownloadIcon = () => <Icon name="download" fill="accent2_dark" height="12px" />;
+const DownloadIcon = props => (
+  <Icon
+    name="download"
+    fill="accent2_dark"
+    height="12px"
+    style={{
+      marginRight: '5px',
+    }}
+  />
+);
 
 async function fetchDictionary(version) {
   const response = await axios.get(`/data/schemas/${version}.json`);
