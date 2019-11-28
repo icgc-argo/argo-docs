72922e4d10df008f4b813c477752aefab295aac6
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-11-28 10:05:15 -0500

    changes

--- website/src/pages/dictionary/index.js
+++ website/src/pages/dictionary/index.js
@@ -16,10 +16,11 @@ const data = require('./data.json');
 import styles from './styles.module.css';
 
 import Typography from '@icgc-argo/uikit/Typography';
-import Select from '@icgc-argo/uikit/form/Select';
-import Button from '@icgc-argo/uikit/Button';
-import DropdownButton from '@icgc-argo/uikit/DropdownButton';
-import Icon from '@icgc-argo/uikit/Icon';
+//import Select from '@icgc-argo/uikit/form/Select';
+///import Button from '@icgc-argo/uikit/Button';
+//import DropdownButton from '@icgc-argo/uikit/DropdownButton';
+//import Icon from '@icgc-argo/uikit/Icon';
+//import Input from '@icgc-argo/uikit/form/Input';
 
 import Select from '@icgc-argo/uikit/form/Select';
 import Button from '@icgc-argo/uikit/Button';
@@ -40,6 +41,8 @@ const DownloadIcon = props => (
     }}
   />
 );
+  */
+}
 
 async function fetchDictionary(version) {
   const response = await axios.get(`/data/schemas/${version}.json`);
@@ -71,12 +74,12 @@ function DataDictionary() {
     return (
       <form>
         <div style={{ width: '150px', marginRight: '10px' }}>
-          <Select
+          {/*<Select
             aria-label="version-select"
             value={version}
             options={data.versions.map(d => ({ content: `Version ${d}`, value: d }))}
             onChange={val => updateVersion(val)}
-          />
+          />*/}
         </div>
       </form>
     );
