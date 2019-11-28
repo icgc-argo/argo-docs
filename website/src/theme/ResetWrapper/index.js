c6bbda98cd5c46bbbc7c9f957fc4515c497312c0
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-11-28 17:09:39 -0500

    rename ResetWrapper to StyleWrapper

--- website/src/theme/ResetWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './styles.module.css';
-
-function ResetWrapper(props) {
-  return <div style={styles}>{props.children}</div>;
-}
-
-export default ResetWrapper;
