b6f58585691f75b76dcf87040d2254ad50ee45fb
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-12-10 12:49:53 -0500

    field description styling

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
   tagline: 'ICGC ARGO Docs',
 
   scripts: [],
-  stylesheets: ['https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,600&display=swap'],
+  stylesheets: ['https://fonts.googleapis.com/css?family=Source+Code+Pro|Work+Sans&display=swap'],
 
   plugins: [path.resolve(__dirname, './svg-plugin')],
 
