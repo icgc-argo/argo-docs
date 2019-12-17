4c38ae21b309bb722ece693ef2930489b969e110
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-12-17 11:18:50 +0000

    cleanup

--- website/src/pages/dictionary/Schema/index.js
+++ website/src/pages/dictionary/Schema/index.js
@@ -5,24 +5,6 @@ import styles from './styles.module.css';
 import Tag from '@icgc-argo/uikit/Tag';
 import CodeList from './CodeList';
 
-const renderValuesList = list => {
-  const maxEnumLength = 5;
-  const fullOutput = list.map(item => (
-    <p className={styles.fieldEnumValue}>
-      <strong>{item}</strong>
-    </p>
-  ));
-
-  return fullOutput.length > maxEnumLength
-    ? [
-        ...fullOutput.slice(0, maxEnumLength),
-        <p className={styles.fieldEnumValue}>
-          <strong>{fullOutput.length - maxEnumLength} more...</strong>
-        </p>,
-      ]
-    : fullOutput;
-};
-
 const formatFieldType = value => {
   switch (value) {
     case 'string':
@@ -30,29 +12,6 @@ const formatFieldType = value => {
     default:
       return value.toUpperCase();
   }
-};
-
-const FieldRow = field => {
-  return (
-    <tr>
-      <td className={styles.schemaTable_column}>
-        <h3>{field.name}</h3>
-        <p className={styles.fieldDescription}>{field.description}</p>
-      </td>
-      <td className={styles.schemaTable_column}>
-        {field.restrictions && field.restrictions.required && (
-          <div className={styles.requiredTag}>Required</div>
-        )}
-      </td>
-      <td className={styles.schemaTable_column}>{formatFieldType(field.valueType)}</td>
-      <td className={styles.schemaTable_column}>
-        {field.restrictions &&
-          field.restrictions.codeList &&
-          renderValuesList(field.restrictions.codeList)}
-      </td>
-      <td className={styles.schemaTable_column}></td>
-    </tr>
-  );
 };
 
 const RegexRestriction = ({ regex }) => (
@@ -81,8 +40,6 @@ const FieldsTag = ({ fieldCount }) => (
 );
 
 const Schema = ({ schema, key }) => {
-  console.log('schema', schema);
-
   const fields = schema.fields.map(field => {
     /**
      * need to pass in state for Cell rendering
@@ -139,7 +96,7 @@ const Schema = ({ schema, key }) => {
   const ext = 'tsv';
 
   return (
-    <div>
+    <div className={styles.schema}>
       <h2 className={styles.schemaTitle}>{schema.name}</h2>
       <FieldsTag fieldCount={fields.length} />
       <div className={styles.fieldExample}>
