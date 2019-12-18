537041a08e74cbc81619a9896c1229a904b27fcc
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-12-18 17:31:12 +0000

    isolate highlighting regex functionality into self contained react component

--- website/src/pages/dictionary/Schema/Regex.js
+++ website/src/pages/dictionary/Schema/Regex.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import RegexColorize from 'regex-colorize';
-import 'regex-colorize/themes/nobg.css'; // If you import a css file in your library
 import styles from './styles.module.css';
+import HighlightedRegex from './HighlightedRegex';
 
 const Regex = ({ regex }) => {
-  const rgx = new RegexColorize();
-  rgx.colorizeAll();
-
   return (
     <div className={styles.regexRestriction}>
       <div>Values must meet the regular expression</div>
-      <div className={`${styles.regex} regex`}>{regex}</div>
+      <HighlightedRegex regex={regex} />
       <br />
       <div>Examples:</div>
       <div>
