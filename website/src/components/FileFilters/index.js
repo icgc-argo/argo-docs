a32fda3752f778f5951b6737831c5f19eab09419
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-01-14 16:31:37 -0500

    hook up filter bar

--- website/src/components/FileFilters/index.js
+++ website/src/components/FileFilters/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Select from '@icgc-argo/uikit/form/Select';
 import Input from '@icgc-argo/uikit/form/Input';
-
 import styles from './styles.module.css';
 import Typography from '@icgc-argo/uikit/Typography';
 
 const FileFilters = ({
-  files = 14,
-  fields = 64,
-  attributes = ['All', 'Required'],
+  files = 0,
+  fields = 0,
+  dataTiers = [],
+  dataAttributes = [],
   onSearch = e => console.log(e.target.val),
 }) => (
   <Typography variant="data" color="#151c3d">
     <div className={styles.fileFilters}>
       {`${files} files > ${fields} fields`}
       <div className={styles.dataSelectors}>
-        Data Tier: <Select options={attributes} size="sm" />
-        Attribute: <Select options={attributes} size="sm" />
+        Data Tier: <Select options={dataTiers} size="sm" />
+        Attribute: <Select options={dataAttributes} size="sm" />
         <Input
           onChange={onSearch}
           placeholder="Search Dictionary..."
