c2619096fc1310053ef3b14490880b075e6d6091
Minh <hlminh2000@gmail.com>
2020-01-23 19:11:46 -0500

    :sparkles: sets up zooming and panning

--- website/src/components/TreeView/Index.js
+++ website/src/components/TreeView/Index.js
@@ -1,5 +1,16 @@
 import React, { useState, createRef, useEffect } from 'react';
+import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
+import ZoomPanContainer from './ZoomPanContainer';
 
-const TreeView = () => <div>yo!!!</div>;
+const TreeView = () => {
+  return (
+    <ZoomPanContainer>
+      <img
+        src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg"
+        alt="Italian Trulli"
+      />
+    </ZoomPanContainer>
+  );
+};
 
 export default TreeView;
