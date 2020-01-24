b60e4436364d386c590c5e844961692b17dd78a9
Minh <hlminh2000@gmail.com>
2020-01-24 11:42:08 -0500

    :sparkles: wip zoom pan implementation

--- website/src/components/TreeView/ZoomPanContainer.js
+++ website/src/components/TreeView/ZoomPanContainer.js
@@ -6,7 +6,7 @@ const MAX_ZOOM = 1.5;
 const ZOOM_STEP = 0.05;
 
 export default ({ children, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, zoomStep = ZOOM_STEP }) => {
-  const [zoom, setZoom] = React.useState(1);
+  const [scaleFactor, setScaleFactor] = React.useState(1);
   const [position, setPosition] = React.useState({
     x: 0,
     y: 0,
@@ -15,17 +15,23 @@ export default ({ children, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, zoomStep = Z
     x: 0,
     y: 0,
   });
+  const [objectLocalMousedownPosition, setObjectLocalMousedownPosition] = React.useState({
+    x: 0,
+    y: 0,
+  });
+  const [isDragging, setDragging] = React.useState(false);
   const theme = useTheme();
 
   const containerRef = React.createRef();
+  const objectElementRef = React.createRef();
   React.useEffect(() => {
     const element = containerRef.current;
     const onScroll = e => {
       e.preventDefault();
       if (e.deltaY > 0) {
-        setZoom(Math.min(zoom + zoomStep, maxZoom));
+        setScaleFactor(Math.min(scaleFactor + zoomStep, maxZoom));
       } else {
-        setZoom(Math.max(zoom - zoomStep, minZoom));
+        setScaleFactor(Math.max(scaleFactor - zoomStep, minZoom));
       }
     };
     element.addEventListener('mousewheel', onScroll);
@@ -35,14 +41,46 @@ export default ({ children, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, zoomStep = Z
   });
 
   const onMouseDown = e => {
-    // console.log(e);
-  };
-  const onMouseUp = e => {
-    // console.log(e);
+    const eventLocalX = e.pageX - containerRef.current.offsetLeft;
+    const eventLocalY = e.pageY - containerRef.current.offsetTop;
+    e.preventDefault();
+    setDragging(true);
+    setObjectLocalMousedownPosition({
+      x: eventLocalX - position.x,
+      y: eventLocalY - position.y,
+    });
+    setMouseDownPosition({
+      x: eventLocalX,
+      y: eventLocalY,
+    });
   };
   React.useEffect(() => {
+    const onMouseUp = e => {
+      setDragging(false);
+    };
+    window.addEventListener('mouseup', onMouseUp);
+    return () => window.removeEventListener('mouseup', onMouseUp);
+  });
+  React.useEffect(() => {
+    const containerElement = containerRef.current;
+    const objectElement = objectElementRef.current;
     const onMouseMove = e => {
-      // console.log(e);
+      if (isDragging) {
+        const eventLocalX = e.pageX - containerElement.offsetLeft;
+        const eventLocalY = e.pageY - containerElement.offsetTop;
+        const delta = {
+          x: eventLocalX - mouseDownPosition.x,
+          y: eventLocalY - mouseDownPosition.y,
+        };
+        const zoomOffset = {
+          x: -0.5 * (1 - scaleFactor) * objectElement.clientWidth,
+          y: -0.5 * (1 - scaleFactor) * objectElement.clientHeight,
+        };
+        setPosition({
+          x: mouseDownPosition.x + zoomOffset.x - objectLocalMousedownPosition.x + delta.x,
+          y: mouseDownPosition.y + zoomOffset.y - objectLocalMousedownPosition.y + delta.y,
+        });
+      }
     };
     window.addEventListener('mousemove', onMouseMove);
     return () => window.removeEventListener('mousemove', onMouseMove);
@@ -56,27 +94,33 @@ export default ({ children, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, zoomStep = Z
         height: '800px',
         overflow: 'hidden',
         transform: 'scale(1)',
+        cursor: 'grab',
       }}
       onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
     >
-      <input
-        type="range"
-        min={minZoom}
-        max={maxZoom}
-        step={zoomStep}
-        value={zoom}
+      <div
         style={{
           zIndex: 1,
           position: 'absolute',
         }}
-        onChange={e => setZoom(Number(e.target.value))}
-      />
+      >
+        <input
+          type="range"
+          min={minZoom}
+          max={maxZoom}
+          step={zoomStep}
+          value={scaleFactor}
+          onChange={e => setScaleFactor(Number(e.target.value))}
+          onMouseDown={e => e.stopPropagation()}
+        />{' '}
+        {scaleFactor}x
+      </div>
       <div
         id="zoomer"
+        ref={objectElementRef}
         style={{
           border: 'solid 2px green',
-          transform: `scale(${zoom})`,
+          transform: `scale(${scaleFactor})`,
           height: '100%',
           width: '100%',
           position: 'absolute',
