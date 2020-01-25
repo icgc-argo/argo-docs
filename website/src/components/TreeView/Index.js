09af4c9a66411d51d6c92a099572fd282dac156c
Minh <hlminh2000@gmail.com>
2020-01-25 16:43:34 -0500

    :heavy_plus_sign: switch pan and zoom implementation to use third-party component

--- website/src/components/TreeView/Index.js
+++ website/src/components/TreeView/Index.js
@@ -3,12 +3,27 @@ import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
 import ZoomPanContainer from './ZoomPanContainer';
 
 const TreeView = () => {
+  const [shown, setShown] = React.useState(false);
+  const onClick = () => {
+    setShown(!shown);
+  };
   return (
     <ZoomPanContainer>
-      <img
-        src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg"
-        alt="Italian Trulli"
-      />
+      <div>
+        <img
+          src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg"
+          alt="Italian Trulli"
+        />
+        {shown && (
+          <img
+            src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg"
+            alt="Italian Trulli"
+          />
+        )}
+        <button style={{ position: 'absolute' }} onClick={onClick}>
+          YO!!!
+        </button>
+      </div>
     </ZoomPanContainer>
   );
 };
--- website/src/components/TreeView/ZoomPanContainer.js
+++ website/src/components/TreeView/ZoomPanContainer.js
@@ -1,135 +1,83 @@
 import React from 'react';
 import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
+import debounce from 'lodash/debounce';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 const MIN_ZOOM = 0.3;
 const MAX_ZOOM = 1.5;
 const ZOOM_STEP = 0.05;
 
 export default ({ children, minZoom = MIN_ZOOM, maxZoom = MAX_ZOOM, zoomStep = ZOOM_STEP }) => {
-  const [scaleFactor, setScaleFactor] = React.useState(1);
-  const [position, setPosition] = React.useState({
-    x: 0,
-    y: 0,
-  });
-  const [mouseDownPosition, setMouseDownPosition] = React.useState({
-    x: 0,
-    y: 0,
-  });
-  const [objectLocalMousedownPosition, setObjectLocalMousedownPosition] = React.useState({
-    x: 0,
-    y: 0,
-  });
-  const [isDragging, setDragging] = React.useState(false);
   const theme = useTheme();
-
-  const containerRef = React.createRef();
-  const objectElementRef = React.createRef();
-  React.useEffect(() => {
-    const element = containerRef.current;
-    const onScroll = e => {
-      e.preventDefault();
-      if (e.deltaY > 0) {
-        setScaleFactor(Math.min(scaleFactor + zoomStep, maxZoom));
-      } else {
-        setScaleFactor(Math.max(scaleFactor - zoomStep, minZoom));
-      }
-    };
-    element.addEventListener('mousewheel', onScroll);
-    return () => {
-      element.removeEventListener('mousewheel', onScroll);
-    };
-  });
-
-  const onMouseDown = e => {
-    const eventLocalX = e.pageX - containerRef.current.offsetLeft;
-    const eventLocalY = e.pageY - containerRef.current.offsetTop;
-    e.preventDefault();
-    setDragging(true);
-    setObjectLocalMousedownPosition({
-      x: eventLocalX - position.x,
-      y: eventLocalY - position.y,
-    });
-    setMouseDownPosition({
-      x: eventLocalX,
-      y: eventLocalY,
-    });
+  const DEFAULT_SCALE = 1;
+  const [sliderValue, setSliderValue] = React.useState(DEFAULT_SCALE);
+  const onWheel = ({ scale }) => {
+    setSliderValue(scale);
   };
-  React.useEffect(() => {
-    const onMouseUp = e => {
-      setDragging(false);
-    };
-    window.addEventListener('mouseup', onMouseUp);
-    return () => window.removeEventListener('mouseup', onMouseUp);
-  });
-  React.useEffect(() => {
-    const containerElement = containerRef.current;
-    const onMouseMove = e => {
-      if (isDragging) {
-        const eventLocalX = e.pageX - containerElement.offsetLeft;
-        const eventLocalY = e.pageY - containerElement.offsetTop;
-        const delta = {
-          x: eventLocalX - mouseDownPosition.x,
-          y: eventLocalY - mouseDownPosition.y,
-        };
-        setPosition({
-          x: mouseDownPosition.x - objectLocalMousedownPosition.x + delta.x,
-          y: mouseDownPosition.y - objectLocalMousedownPosition.y + delta.y,
-        });
-      }
-    };
-    window.addEventListener('mousemove', onMouseMove);
-    return () => window.removeEventListener('mousemove', onMouseMove);
-  });
-
   return (
     <div
-      ref={containerRef}
       style={{
         border: `solid 1px ${theme.colors.grey_2}`,
-        height: '800px',
+        transform: `scale(1)`,
+        height: `800px`,
         overflow: 'hidden',
-        transform: 'scale(1)',
-        cursor: 'grab',
       }}
-      onMouseDown={onMouseDown}
     >
-      <div
-        style={{
-          zIndex: 1,
-          position: 'absolute',
+      <TransformWrapper
+        defaultScale={DEFAULT_SCALE}
+        defaultPositionX={0}
+        defaultPositionY={0}
+        options={{
+          minScale: minZoom,
+          maxScale: maxZoom,
+          limitToBounds: false,
+          limitToWrapper: true,
+        }}
+        onWheel={onWheel}
+        wheel={{
+          step: 20,
         }}
       >
-        <button onClick={() => setScaleFactor(1)}>reset</button>{' '}
-        <input
-          type="range"
-          min={minZoom}
-          max={maxZoom}
-          step={zoomStep}
-          value={scaleFactor}
-          onChange={e => setScaleFactor(Number(e.target.value))}
-          onMouseDown={e => e.stopPropagation()}
-        />{' '}
-        {scaleFactor}x
-      </div>
-      <div
-        id="zoomer"
-        ref={objectElementRef}
-        style={{
-          height: '100%',
-          width: '100%',
-          position: 'absolute',
-          left: position.x,
-          top: position.y,
+        {({ resetTransform, setScale }) => {
+          const applySliderValue = debounce(value => {
+            setScale(value);
+          }, 500);
+          return (
+            <React.Fragment>
+              <div
+                className="tools"
+                style={{
+                  position: 'absolute',
+                  zIndex: 1,
+                  display: 'flex',
+                }}
+              >
+                <input
+                  type="range"
+                  min={minZoom}
+                  max={maxZoom}
+                  step={zoomStep}
+                  value={sliderValue}
+                  onChange={e => {
+                    const value = Number(e.target.value);
+                    setSliderValue(value);
+                    applySliderValue(value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    resetTransform();
+                    setSliderValue(DEFAULT_SCALE);
+                  }}
+                >
+                  reset
+                </button>
+              </div>
+              <TransformComponent>{children}</TransformComponent>
+            </React.Fragment>
+          );
         }}
-      >
-        <div
-          style={{
-            transform: `scale(${scaleFactor})`,
-          }}
-        >
-          {children}
-        </div>
-      </div>
+      </TransformWrapper>
     </div>
   );
 };
