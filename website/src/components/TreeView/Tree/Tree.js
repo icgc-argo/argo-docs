c34138574c9705f6ab9e1ee92c6f1a1b6e45332f
Minh <hlminh2000@gmail.com>
2020-01-25 21:58:31 -0500

    :sparkles: adds demmo tree

---
+++ website/src/components/TreeView/Tree/Tree.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+import TreeNode from './TreeNode';
+
+const RootContainer = styled('ul')`
+  padding-inline-start: 0;
+  margin: 0;
+`;
+
+const StyledRootContainer = styled(RootContainer)`
+  --line-height: ${({ lineHeight }) => lineHeight};
+  --line-width: ${({ lineWidth }) => lineWidth};
+  --line-color: ${({ lineColor }) => lineColor};
+  --line-border-radius: ${({ lineBorderRadius }) => lineBorderRadius};
+  --node-padding: ${({ nodePadding }) => nodePadding};
+  --tree-line-height: var(--line-height, 20px);
+  --tree-line-width: var(--line-width, 1px);
+  --tree-line-color: var(--line-color, black);
+  --tree-line-border-radius: var(--line-border-radius, 5px);
+  --tree-node-padding: var(--node-padding, 5px);
+`;
+
+function Tree({ children, label, ...props }) {
+  return (
+    <StyledRootContainer {...props}>
+      <TreeNode label={label}>{children}</TreeNode>
+    </StyledRootContainer>
+  );
+}
+
+export default React.memo(Tree);
---
+++ website/src/components/TreeView/Tree/TreeNode.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+const NodeContainer = styled('li')`
+  flex: auto;
+  text-align: center;
+  list-style-type: none;
+  position: relative;
+  padding-top: var(--tree-line-height);
+  padding-right: var(--tree-node-padding);
+  padding-bottom: 0;
+  padding-left: var(--tree-node-padding);
+  ::before,
+  ::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 50%;
+    width: 50%;
+    border-top: var(--tree-line-width) solid var(--tree-line-color);
+    height: var(--tree-line-height);
+  }
+  ::after {
+    left: 50%;
+    border-left: var(--tree-line-width) solid var(--tree-line-color);
+  }
+  :only-child {
+    padding: 0;
+    ::after,
+    :before {
+      display: none;
+    }
+  }
+  :first-of-type {
+    ::before {
+      border: 0 none;
+    }
+    ::after {
+      border-radius: var(--tree-line-border-radius) 0 0 0;
+    }
+  }
+  :last-of-type {
+    ::before {
+      border-right: var(--tree-line-width) solid var(--tree-line-color);
+      border-radius: 0 var(--tree-line-border-radius) 0 0;
+    }
+    ::after {
+      border: 0 none;
+    }
+  }
+`;
+
+const ChildrenContainer = styled('ul')`
+  display: flex;
+  padding-inline-start: 0;
+  padding-top: var(--tree-line-height);
+  position: relative;
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 0;
+    border-left: var(--tree-line-width) solid var(--tree-line-color);
+    height: var(--tree-line-height);
+  }
+`;
+
+function TreeNode({ children, label, className }) {
+  return (
+    <NodeContainer className={className}>
+      {label}
+      {React.Children.count(children) > 0 && <ChildrenContainer>{children}</ChildrenContainer>}
+    </NodeContainer>
+  );
+}
+
+export default TreeNode;
