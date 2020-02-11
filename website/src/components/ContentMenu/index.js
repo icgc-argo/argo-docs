65e7eca990413e90f9d4ab334b7e85c273378f98
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-02-11 18:12:17 -0500

    remove section component

--- website/src/components/ContentMenu/index.js
+++ website/src/components/ContentMenu/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import { useMenuHighlight } from './hooks';
 import Menu from './menu';
 
-const DEFAULT_SECTION_CLASSNAME = 'menu-content-section';
-const DEFAULT_ANCHOR_CLASSNAME = 'menu-anchor';
-
 const SchemaMenu = ({ contents, ...props }) => {
   const schemaRefs = contents.map(schema => schema.contentRef);
   //useMenuHighlight(schemaRefs, DEFAULT_ANCHOR_CLASSNAME, scrollYOffset);
   return <Menu contents={contents} {...props} />;
 };
-
-/**
- * Content section, should wrap whole section
- */
-export const ContentSection = ({ title, children }) => (
-  <div className={DEFAULT_SECTION_CLASSNAME}>{children}</div>
-);
 
 export default SchemaMenu;
