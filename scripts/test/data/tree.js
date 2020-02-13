cbdf7d13af413ec23807c87e847799a2bed5d960
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-02-13 15:35:14 -0500

    add package - chai

--- scripts/test/data/tree.js
+++ scripts/test/data/tree.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   name: 'sample_registration',
   required: true,
   fields: [
--- scripts/test/generateData.js
+++ scripts/test/generateData.js
@@ -1,6 +1,11 @@
-var assert = require('assert');
+const expect = require('chai').expect;
+const { generateTreeData } = require('../generateData');
+const sampleSchemaData = require('./data/schemaData');
+const sampleTreeData = require('./data/tree');
+
 describe('Tree Data', function() {
-  it('should return -1 when the value is not present', function() {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it('should generate tree data', function() {
+    const testTreeData = generateTreeData(sampleSchemaData);
+    expect(sampleTreeData).to.eql(testTreeData);
   });
 });
