3b212396df0cd627548dfe13925442e4af3b9e9d
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-03-17 11:30:38 -0400

    remove commented code

--- website/src/components/TreeView/Tree/TreeNode.js
+++ website/src/components/TreeView/Tree/TreeNode.js
@@ -6,48 +6,6 @@ const NodeContainer = styled('li')`
   text-align: center;
   list-style-type: none;
   position: relative;
-  /* padding-top: var(--tree-line-height);
-  padding-right: var(--tree-node-padding);
-  padding-bottom: 0;
-  padding-left: var(--tree-node-padding); */
-  /* ::before,
-  ::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 50%;
-    width: 50%;
-    border-top: var(--tree-line-width) solid var(--tree-line-color);
-    height: var(--tree-line-height);
-  }
-  ::after {
-    left: 50%;
-    border-left: var(--tree-line-width) solid var(--tree-line-color);
-  }
-  :only-child {
-    padding: 0;
-    ::after,
-    :before {
-      display: none;
-    }
-  }
-  :first-of-type {
-    ::before {
-      border: 0 none;
-    }
-    ::after {
-      border-radius: var(--tree-line-border-radius) 0 0 0;
-    }
-  }
-  :last-of-type {
-    ::before {
-      border-right: var(--tree-line-width) solid var(--tree-line-color);
-      border-radius: 0 var(--tree-line-border-radius) 0 0;
-    }
-    ::after {
-      border: 0 none;
-    }
-  } */
 
   display: flex;
   flex-direction: row;
@@ -102,17 +60,7 @@ const ChildrenContainer = styled('ul')`
   padding-inline-start: 0;
   padding-top: var(--tree-line-height);
   position: relative;
-  /* ::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 0;
-    border-left: var(--tree-line-width) solid var(--tree-line-color);
-    height: var(--tree-line-height);
-  } */
 
-  /* border: solid 2px green; */
   flex-direction: column;
   padding-left: var(--tree-line-height);
   padding-top: 0px;
