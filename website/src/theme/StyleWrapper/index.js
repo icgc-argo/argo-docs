d87eaea5a99a28a99dc416931fc25f6552f19cb5
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-03-31 11:26:21 -0400

    Bug/#155 banner jump (#175)
    * reduce scope of StyleWrapper css
    
    * move StyleWrapper into components folder

--- website/src/theme/StyleWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './styles.module.css';
-
-function StyleWrapper(props) {
-  return <div style={styles}>{props.children}</div>;
-}
-
-export default StyleWrapper;
