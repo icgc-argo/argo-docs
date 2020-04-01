68e69d21fbf0872abdd01bc4440ee483b99a7a4e
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-04-01 14:56:02 -0400

    lazy load Docsearch

--- website/src/hooks/useAlgolia.js
+++ website/src/hooks/useAlgolia.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useHistory } from '@docusaurus/router';
-import docsearch from 'docsearch.js';
 
 /**
  * Expects id selector for input
@@ -14,28 +13,33 @@ const useAlgolia = (inputRef, options = {}) => {
   }, []);
 
   if (!process.env.ALGOLIA_API_KEY || !process.env.ALGOLIA_INDEX) {
-    console.error('Search not configured');
-  } else if (currentInput) {
-    docsearch({
-      // debug: true,
-      apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: process.env.ALGOLIA_INDEX,
-      inputSelector: `#${inputRef.current.id}`,
-      algoliaOptions: options,
-      // Override algolia's default selection event, allowing us to do client-side
-      // navigation and avoiding a full page refresh.
-      handleSelected: (_input, _event, suggestion) => {
-        // Use an anchor tag to parse the absolute url into a relative url
-        // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
-        const a = document.createElement('a');
-        a.href = suggestion.url;
+    console.warn('Search not configured');
+  } else if (currentInput && window) {
+    // lazy load because docsearch module isn't SSR friendly
+    import('docsearch.js').then(({ default: docsearch }) => {
+      docsearch({
+        // debug: true,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX,
+        inputSelector: `#${inputRef.current.id}`,
+        algoliaOptions: options,
+        // Override algolia's default selection event, allowing us to do client-side
+        // navigation and avoiding a full page refresh.
+        handleSelected: (_input, _event, suggestion) => {
+          // Use an anchor tag to parse the absolute url into a relative url
+          // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
+          const a = document.createElement('a');
+          a.href = suggestion.url;
 
-        // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
-        // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
-        const routePath = `#__docusaurus` === a.hash ? `${a.pathname}` : `${a.pathname}${a.hash}`;
-        history.push(routePath);
-      },
+          // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
+          // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
+          const routePath = `#__docusaurus` === a.hash ? `${a.pathname}` : `${a.pathname}${a.hash}`;
+          history.push(routePath);
+        },
+      });
     });
+  } else {
+    console.warn('Search not available');
   }
 };
 
