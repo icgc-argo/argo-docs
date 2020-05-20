0899acc18a0d8dec0686d7f302733d673f7f884a
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-05-20 17:54:25 -0400

    Feat/enable search ui (#214)
    * enable Search boxes
    
    * check if search is available hook
    
    * remove unused state check
    
    * catch docsearch init error
    
    * change search available check to constant

--- website/src/hooks/useAlgolia.js
+++ website/src/hooks/useAlgolia.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useHistory } from '@docusaurus/router';
+import { isSearchAvailable } from '../utils';
 
 /**
  * Expects id selector for input
@@ -7,39 +7,39 @@ import { useHistory } from '@docusaurus/router';
 const useAlgolia = (inputRef, options = {}) => {
   const history = useHistory();
 
-  const [currentInput, setCurrentInput] = React.useState();
-  React.useEffect(() => {
-    setCurrentInput(inputRef.current);
-  }, []);
-
-  if (!process.env.ALGOLIA_API_KEY || !process.env.ALGOLIA_INDEX) {
+  if (!isSearchAvailable) {
     console.warn('Search not configured');
-  } else if (currentInput && window) {
-    // lazy load because docsearch module isn't SSR friendly
-    import('docsearch.js').then(({ default: docsearch }) => {
-      docsearch({
-        // debug: true,
-        apiKey: process.env.ALGOLIA_API_KEY,
-        indexName: process.env.ALGOLIA_INDEX,
-        inputSelector: `#${inputRef.current.id}`,
-        algoliaOptions: options,
-        // Override algolia's default selection event, allowing us to do client-side
-        // navigation and avoiding a full page refresh.
-        handleSelected: (_input, _event, suggestion) => {
-          // Use an anchor tag to parse the absolute url into a relative url
-          // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
-          const a = document.createElement('a');
-          a.href = suggestion.url;
-
-          // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
-          // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
-          const routePath = `#__docusaurus` === a.hash ? `${a.pathname}` : `${a.pathname}${a.hash}`;
-          history.push(routePath);
-        },
-      });
-    });
+    return false;
   } else {
-    console.warn('Search not available');
+    // lazy load because docsearch module isn't SSR friendly
+    import('docsearch.js')
+      .then(({ default: docsearch }) => {
+        docsearch({
+          // debug: true,
+          apiKey: process.env.ALGOLIA_API_KEY,
+          indexName: process.env.ALGOLIA_INDEX,
+          inputSelector: `#${inputRef.current.id}`,
+          algoliaOptions: options,
+          // Override algolia's default selection event, allowing us to do client-side
+          // navigation and avoiding a full page refresh.
+          handleSelected: (_input, _event, suggestion) => {
+            // Use an anchor tag to parse the absolute url into a relative url
+            // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
+            const a = document.createElement('a');
+            a.href = suggestion.url;
+
+            // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
+            // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
+            const routePath =
+              `#__docusaurus` === a.hash ? `${a.pathname}` : `${a.pathname}${a.hash}`;
+            history.push(routePath);
+          },
+        });
+      })
+      .catch((e) => {
+        console.warn('Search not available');
+        return false;
+      });
   }
 };
 
