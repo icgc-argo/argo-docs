601ebd43cca32a70c4a3e96b3f159c24f2499c03
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-07-22 16:11:46 -0400

    change FileFilters to TS file

--- website/src/components/FileFilters/index.tsx
+++ website/src/components/FileFilters/index.tsx
@@ -5,84 +5,64 @@
  * You should have received a copy of the GNU Affero General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  *
  */
 
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
 import React from 'react';
-import SelectComp from '../../components/Select';
+import Select from '@icgc-argo/uikit/form/Select';
+import Input from '@icgc-argo/uikit/form/Input';
+import styles from './styles.module.css';
 import Typography from '@icgc-argo/uikit/Typography';
+import { styled } from '@icgc-argo/uikit';
 import debounce from 'lodash/debounce';
-import startCase from 'lodash/startCase';
-import { css } from '@emotion/core';
-import { Filter } from './components';
 
-export const NO_ACTIVE_FILTER: string = 'no_active_filter';
-export const DEFAULT_FILTER: FilterSelect = { content: 'All', value: NO_ACTIVE_FILTER };
+export const NO_ACTIVE_FILTER = 'no_active_filter';
+export const DEFAULT_FILTER = [{ content: 'All', value: NO_ACTIVE_FILTER }];
+
+const StyledSelect = styled(Select)`
+  min-width: 190px;
+`;
 
 const FileFilters = ({
-  dataTiers = [], // change to tiers
-  dataAttributes = [], // change to attributes
-  comparisons = [],
+  dataTiers = [],
+  dataAttributes = [],
   searchParams = {},
-  onFilter,
-}: {
-  dataTiers: Array<FilterSelect>;
-  dataAttributes: Array<FilterSelect>;
-  comparisons: Array<FilterSelect>;
-  searchParams: { [key: string]: string };
-  onFilter: (any) => void;
+  onSearch = e => console.log(e.target.val),
 }) => {
   // update search params
-  const onSelect = (filterName) => (value) =>
-    onFilter({ ...searchParams, ...{ [filterName]: value } });
+  const onSelect = filterName => value => onSearch({ ...searchParams, ...{ [filterName]: value } });
 
-  //const [inputValue, setInputValue] = React.useState('');
-  //const applySearch = debounce(onSearch, 500);
+  const [inputValue, setInputValue] = React.useState('');
+  const applySearch = debounce(onSearch, 500);
   return (
     <Typography variant="data" color="#151c3d">
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
-        `}
-      >
-        <Filter
-          label="Comparison"
-          ariaLabel="Comparison Attribute Select"
-          options={[DEFAULT_FILTER, ...comparisons]}
-          value={searchParams.comparison}
-          onChange={onSelect('comparison')}
-        />
-
-        <Filter
-          label="Data Tier"
-          ariaLabel="Data Tier Select"
-          options={[DEFAULT_FILTER, ...dataTiers]}
-          value={searchParams.tier}
-          onChange={onSelect('tier')}
-        />
-
-        <Filter
-          label="Attribute"
-          ariaLabel="Data Attribute Select"
-          options={[DEFAULT_FILTER, ...dataAttributes]}
-          value={searchParams.attribute}
-          onChange={onSelect('attribute')}
-        />
-
-        {/*<Input
+      <div className={styles.fileFilters}>
+        <div className={styles.dataSelectors}>
+          Data Tier:{' '}
+          <StyledSelect
+            options={dataTiers}
+            value={searchParams.tier}
+            onChange={onSelect('tier')}
+            size="sm"
+          />
+          Attribute:{' '}
+          <StyledSelect
+            options={dataAttributes}
+            value={searchParams.attribute}
+            onChange={onSelect('attribute')}
+            size="sm"
+          />
+          {/*<Input
             onChange={e => {
               setInputValue(e.target.value);
               applySearch(e.target.value);
@@ -92,20 +72,10 @@ const FileFilters = ({
             preset="search"
             className={styles.search}
           />*/}
+        </div>
       </div>
     </Typography>
   );
 };
-
-export const comparisonFilterDisplay = {
-  updates: 'Updated fields',
-  deletions: 'Deleted fields',
-  additions: 'Added fields',
-};
-
-export const generateFilter = (item: string): FilterSelect => ({
-  content: startCase(item),
-  value: item,
-});
 
 export default FileFilters;
