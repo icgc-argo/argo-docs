60246015344fb7535726a4e9074690ac5c8f8516
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-07-22 15:38:18 -0400

    update theme

--- website/src/styles/theme/icgc-argo/index.ts
+++ website/src/styles/theme/icgc-argo/index.ts
@@ -1,6 +1,23 @@
-import theme from '@icgc-argo/uikit/theme/defaultTheme';
+import argo from '@icgc-argo/uikit/theme/defaultTheme';
 
-export default {
-  colors: theme.colors,
-  typography: theme.typography,
+const argoColors = argo.colors;
+
+export type DiffColors = { [key: string]: string };
+const diffColors: DiffColors = {
+  accent1: argoColors.accent1_dimmed,
+  accent2: argoColors.warning_dark,
+  accent3: argoColors.error,
 };
+
+/**
+ * This project very heavily depedendant on UIKIT right now, which is dependant on certain theme format
+ * for now keep passing through all of uikit default theme
+ */
+const theme = {
+  ...argo,
+  diffColors,
+};
+
+export default theme;
+
+export type Theme = typeof theme;
