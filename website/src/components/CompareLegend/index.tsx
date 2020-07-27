be4b52a437fef45cbe14df43e6a30e4cef07ed58
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2020-07-27 14:23:18 -0400

    Feat/#336 diff table updates (#339)
    * change compare filters to compare legend
    
    * remove compare filters from dictionary
    
    * remove compareFilters as memo dependnacy
    
    * update theme
    
    * clean up CompareLegend comp
    
    * fix styling in parent page
    
    * remove babel plugin
    
    * change FileFilters to TS file
    
    * export convienence function to generate Select values
    
    * clean up FilterSelect usage in dictionary page
    
    * start typing FilterSelect comp
    
    * use emotion
    
    * change filter default to be ALL
    
    * add dummy meta file
    
    * added filter for comparisons
    
    * update legend prop
    
    * clean up Filters
    
    * add Select comp
    
    * move type out of comp
    
    * move filter display names into component
    
    * add Table component
    
    * table row highlighting
    
    * star table col
    
    * change color names
    
    * add Table override
    
    * add diff visibility state
    
    * add type to Display component
    
    * styling
    
    * styling and cleanup
    
    * styling
    
    * remove console logs
    
    * cleanup
    
    * change classname to css
    
    * fix types

---
+++ website/src/components/CompareLegend/index.tsx
@@ -0,0 +1,55 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import React from 'react';
+import Icon from '@icgc-argo/uikit/Icon';
+import { css } from '@emotion/core';
+import { useTheme } from 'emotion-theming';
+import { Theme } from '../../styles/theme/icgc-argo';
+
+const Star = ({ fill }: { fill: string }) => (
+  <Icon
+    name="star"
+    fill={fill}
+    width="14px"
+    css={css`
+      margin-right: 4px;
+      margin-left: 8px;
+    `}
+  />
+);
+
+const CompareLegend = ({
+  comparison,
+  styles,
+}: {
+  comparison: {
+    additions: number;
+    updates: number;
+    deletions: number;
+  };
+  styles?: any;
+}) => {
+  const theme: Theme = useTheme();
+  const diffColors = theme.diffColors;
+  const { additions, deletions, updates } = comparison;
+  return (
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        font-weight: bold;
+        ${styles}
+      `}
+    >
+      <Star fill={diffColors.created} />
+      {`${additions} new fields`}
+      <Star fill={diffColors.updated} />
+      {`${updates} updated fields`}
+      <Star fill={diffColors.deleted} />
+      {`${deletions} deleted fields`}
+    </div>
+  );
+};
+
+export default CompareLegend;
---
+++ website/src/components/FileFilters/index.tsx
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2020 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of the GNU Affero General Public License v3.0.
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ */
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import React from 'react';
+import SelectComp from '../../components/Select';
+import Typography from '@icgc-argo/uikit/Typography';
+import debounce from 'lodash/debounce';
+import startCase from 'lodash/startCase';
+import { css } from '@emotion/core';
+import { Filter } from './components';
+
+export const NO_ACTIVE_FILTER: string = 'no_active_filter';
+export const DEFAULT_FILTER: FilterSelect = { content: 'All', value: NO_ACTIVE_FILTER };
+
+const FileFilters = ({
+  dataTiers = [], // change to tiers
+  dataAttributes = [], // change to attributes
+  comparisons = [],
+  searchParams = {},
+  onFilter,
+}: {
+  dataTiers: Array<FilterSelect>;
+  dataAttributes: Array<FilterSelect>;
+  comparisons: Array<FilterSelect>;
+  searchParams: { [key: string]: string };
+  onFilter: (any) => void;
+}) => {
+  // update search params
+  const onSelect = (filterName) => (value) =>
+    onFilter({ ...searchParams, ...{ [filterName]: value } });
+
+  //const [inputValue, setInputValue] = React.useState('');
+  //const applySearch = debounce(onSearch, 500);
+  return (
+    <Typography variant="data" color="#151c3d">
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+        `}
+      >
+        <Filter
+          label="Comparison"
+          ariaLabel="Comparison Attribute Select"
+          options={[DEFAULT_FILTER, ...comparisons]}
+          value={searchParams.comparison}
+          onChange={onSelect('comparison')}
+        />
+
+        <Filter
+          label="Data Tier"
+          ariaLabel="Data Tier Select"
+          options={[DEFAULT_FILTER, ...dataTiers]}
+          value={searchParams.tier}
+          onChange={onSelect('tier')}
+        />
+
+        <Filter
+          label="Attribute"
+          ariaLabel="Data Attribute Select"
+          options={[DEFAULT_FILTER, ...dataAttributes]}
+          value={searchParams.attribute}
+          onChange={onSelect('attribute')}
+        />
+
+        {/*<Input
+            onChange={e => {
+              setInputValue(e.target.value);
+              applySearch(e.target.value);
+            }}
+            value={inputValue}
+            placeholder="Search Dictionary..."
+            preset="search"
+            className={styles.search}
+          />*/}
+      </div>
+    </Typography>
+  );
+};
+
+export const comparisonFilterDisplay = {
+  updates: 'Updated fields',
+  deletions: 'Deleted fields',
+  additions: 'Added fields',
+};
+
+export const generateFilter = (item: string): FilterSelect => ({
+  content: startCase(item),
+  value: item,
+});
+
+export default FileFilters;
