6757bd3052712d99c8ebdae2d7b6e4aa442da04e
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2020-08-13 11:32:54 -0400

    add testing

---
+++ website/src/pages/dictionary/index.test.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+const expect = chai.expect;
+
+describe('Diff data', () => {
+  it('should generate change object for a schema diff', () => {
+    //fse.writeJSONSync('./test.json', result);
+    expect(true).equal(false);
+  });
+});
